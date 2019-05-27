346561e832caecbb69e4f135c09b7d6de5da49de
Victor Lellis <vmlellis@gmail.com>
2019-05-27 01:18:04 -0300

    [FEAT] Add Cards#contains?/2 and update Cards#create_deck/0

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,9 +1,17 @@
 defmodule Cards do
   def create_deck do
-    ["Ace", "Two", "Three"]
+    cards = ["Ace", "Two", "Three"]
+    Enum.map(
+      Enum.to_list(1..Enum.count(cards)),
+      fn i -> String.downcase(Enum.at(cards, i - 1)) end
+    )
   end
 
   def shuffle(deck) do
     Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, String.downcase(card))
   end
 end
